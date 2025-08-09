83cf0b9010462fac1ee9c9d1fe829678303b44cf
Jonathan-R-Anderson <77250765+Jonathan-R-Anderson@users.noreply.github.com>
2025-08-09 19:09:48 -0400

    feat: add linux build option and zsh compatibility

--- build_full.sh
+++ build_full.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
 set -e
 
+# Determine target system from the first argument or SYSTEM env var
+system=${1:-${SYSTEM:-custom}}
+
 # Compile all modules using the full D compiler.
 modules=$(ls src/*.d | tr '\n' ' ')
 
 echo "Compiling modules:" $modules
 
-dmd_cmd=${DC:-anonymos-dmd}
+# Use the Linux system compiler when targeting a Linux host
+if [[ "$system" == "linux" ]]; then
+    dmd_cmd=${DC:-dmd}
+else
+    dmd_cmd=${DC:-anonymos-dmd}
+fi
+
 "$dmd_cmd" -I=. -Isrc $modules -of=interpreter
